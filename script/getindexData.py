805af2cb6dc761f5067c6729eeca4b7b48121b57
felix <805908873@qq.com>
2019-03-28 20:20:30 +0800

    Update getindexData.py

--- script/getindexData.py
+++ script/getindexData.py
@@ -12,10 +12,11 @@ indexInfolist={}
 # 0:name 1：今开 2：昨收  3：现值  4：最高  5：最低  6：  7：  8：成交量（手） 9：成交额
 def get_html(url):
     try:
+        proxy = { "http": "http://z00475199:zpf805908873!@proxy.huawei.com:8080" }
         headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:60.0) Gecko/20100101 Firefox/60.0'}
-        r=requests.get(url,timeout=30, headers = headers)
+        r=requests.get(url,timeout=30, headers = headers, proxies = proxy)
         r.raise_for_status()
-        r.encoding='utf-8'
+        r.encoding='gbk'
         return r.text
     except:
         print('获取数据失败，请检查你的网络连接')
@@ -29,8 +30,10 @@ def main(base_url):
     for indexStr in indexInfolistStr:
         key=indexStr[11:19]
         value=indexStr[21:-2].split(',')
-        indexInfolist[key]=value
-    print(indexInfolist)
+        if key !='':
+            indexInfolist[key]=value
+    res=str(indexInfolist).replace('\'','\"')
+    print(res)
 
 if __name__ == '__main__':
     main(base_url)
