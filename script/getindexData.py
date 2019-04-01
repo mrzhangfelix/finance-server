35fd136bc4aa76ab9b1edc5ceb86930c45c39540
felix <805908873@qq.com>
2019-04-01 08:59:20 +0800

    Update getindexData.py

--- script/getindexData.py
+++ script/getindexData.py
@@ -7,15 +7,15 @@ import time
 import codecs
 import os
 
-base_url = 'http://hq.sinajs.cn/list=sz399001,sh000001,sz399006,sh000300'
+base_url = 'http://api.money.126.net/data/feed/1399001,1399300,0000001,1399006'
+# http://api.money.126.net/data/feed/1399001,1399300,0000001,1399006,HSRANK_COUNT_SHA,HSRANK_COUNT_SZA,HSRANK_COUNT_SH3
 indexInfolist={}
-# 0:name 1：今开 2：昨收  3：现值  4：最高  5：最低  6：  7：  8：成交量（手） 9：成交额
 def get_html(url):
     try:
         headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:60.0) Gecko/20100101 Firefox/60.0'}
         r=requests.get(url,timeout=30, headers = headers)
         r.raise_for_status()
-        r.encoding='gbk'
+        r.encoding='utf-8'
         return r.text
     except:
         print('获取数据失败，请检查你的网络连接')
@@ -23,16 +23,8 @@ def get_html(url):
         return "ERROR"
 
 def main(base_url):
-    indexInfolist={}
     indexInfoStr=get_html(base_url)
-    indexInfolistStr=indexInfoStr.split('\n')
-    for indexStr in indexInfolistStr:
-        key=indexStr[11:19]
-        value=indexStr[21:-2].split(',')
-        if key !='':
-            indexInfolist[key]=value
-    res=str(indexInfolist).replace('\'','\"')
-    print(res)
+    print(indexInfoStr[21:-2])
 
 if __name__ == '__main__':
     main(base_url)
