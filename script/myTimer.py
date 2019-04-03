25e2227f92f5efbd02d0313df4d7d21822e9b549
mrzhangfelix <805908873@qq.com>
2019-04-03 22:17:08 +0800

    0403

--- script/myTimer.py
+++ script/myTimer.py
@@ -7,16 +7,14 @@ def doSth():
     # 假装做这件事情需要一分钟
     time.sleep(60)
 
-
-
-def main(h=9, m=39):
+def main():
     '''h表示设定的小时，m为设定的分钟'''
     while True:
         # 判断是否达到设定时间，例如0:00
         while True:
             now = datetime.datetime.now()
             # 到达设定时间，结束内循环
-            if now.hour==h and now.minute==m:
+            if (now.hour==9 and now.minute==30) or (now.hour==13 and now.minute==1):
                 break
             # 不到时间就等20秒之后再次检测
             time.sleep(40)
