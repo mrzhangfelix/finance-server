4ecf870c3c75de19f36c7447631a0fa8627ffcd8
felix <805908873@qq.com>
2019-04-17 17:26:49 +0800

    Update generateNewFundJson.py

--- script/generateNewFundJson.py
+++ script/generateNewFundJson.py
@@ -6,111 +6,69 @@ import time
 import codecs
 import os
 from utils.requests import get_html
-from utils.time import current_milli_time,get_week
+from utils.time import get_week
+from utils.fund import new_fund,new_fundconf,get_url_amount,get_fundconf
+
 
 base_url = 'http://fundgz.1234567.com.cn/js/{}.js?rt={}'
 sum = 0
 gztime = ''
 module_path = os.path.dirname(__file__)
 
-def get_NewAmount(url,amount):
+def get_NewFund(fund,url,amount):
     try:
         res=get_html(url)
         res=res[8:-2]
         fundinfo=json.loads(res)
-        amountNew=float(fundinfo['gszzl'])*amount/100+amount
-        amountNew=round(amountNew,2)
-        yingli=float(fundinfo['gszzl'])*amount/100
-        yingli=round(yingli,2)
-        zhangfu=float(fundinfo['gszzl'])
-        name=fundinfo['name']
-        # 单位净值
+        amountNew=round(float(fundinfo['gszzl'])*amount/100+amount,2)
+        yingli=round(float(fundinfo['gszzl'])*amount/100,2)
         dwjz=float(fundinfo['dwjz'])
         holdShare=round(amount/dwjz,2)
         # 估算值
         gsz=float(fundinfo['gsz'])
-        amountNow=round(holdShare*gsz,2)
+        week=get_week()
+        fundnew=new_fund()
+        fundnew['fundName'] = fundinfo['name']
+        fundnew['fundcode'] =fund['fundcode']
+        fundnew['add']=fund['add']
+        buychange=int(fund['add'])+int(fund['amountChange'])
+        sellchange=round(float(fund['shareChange'])*gsz,2)
+        fundnew['fundamount'] = amountNew+buychange-sellchange
+        fundnew['yingli'] = yingli
+        fundnew['zhangfu'] = float(fundinfo['gszzl'])
+        fundnew['dwjz'] = dwjz
+        fundnew['holdShare'] = holdShare
+        fundnew['amountNow'] = round(holdShare*gsz,2)
+        fundnew['gusuanzhi'] = gsz
+        fundnew['buyamount7']=fund['buyamount7']
+        fundnew['buyshare7']=fund['buyshare7']
+        fundnew['buyamount7'][week]=buychange
+        fundnew['buyshare7'][week]=round(buychange/gsz,2)
         global gztime
-        # 估值时间
         gztime=fundinfo['gztime']
         global sum
         sum+=yingli
     except BaseException as e:
         print('获取信息失败')
         print(str(traceback.format_exc()))
-    return amountNew,yingli,zhangfu,name,dwjz,holdShare,amountNow,gsz
+    return fundnew
 
 def getfundconf():
-    codelist=[]
-    amountlist=[]
-    url_list=[]
-    amountNewlist=[]
-    yingliList=[]
-    zhangfuList=[]
-    namelist=[]
-    dwjzlist=[]
-    holdSharelist=[]
-    amountNowlist=[]
-    gszlist=[]
-    module_path = os.path.dirname(__file__)
-    with codecs.open(module_path+'\\'+'fund.json', 'r', 'utf-8') as f:
-        fundJson=f.read()
-        fundconf=json.loads(fundJson)
-    for fund in fundconf['fundlist']:
-        codelist.append(fund['fundcode'])
-        amountlist.append(float(fund['fundamount']))
-    for i in codelist:
-        url_list.append(base_url.format(i,current_milli_time()))
+    fundconf=get_fundconf()
+    url_list,amountlist=get_url_amount(fundconf)
     # 获取基金数据
-    for url,amount in zip(url_list,amountlist):
-        amountNew,yingli,zhangfu,name,dwjz,holdShare,amountNow,gsz=get_NewAmount(url,amount)
-        amountNewlist.append(amountNew)
-        yingliList.append(yingli)
-        zhangfuList.append(zhangfu)
-        namelist.append(name)
-        dwjzlist.append(dwjz)
-        holdSharelist.append(holdShare)
-        amountNowlist.append(amountNow)
-        gszlist.append(gsz)
-    # 构造配置文件
-    for fund,amountNew,yingli,zhangfu,name,dwjz,holdShare,amountNow,gsz in zip(fundconf['fundlist'],amountNewlist,yingliList,zhangfuList,namelist,dwjzlist,holdSharelist,amountNowlist,gszlist):
-        if not ('add') in fund:
-            fund['add']=0
-        if not ('amountChange') in fund:
-            fund['amountChange']=0
-        if not ('shareChange') in fund:
-            fund['shareChange']=0
-        if not ('buyamount7') in fund:
-            fund['buyamount7']={}
-        if not ('buyshare7') in fund:
-            fund['buyshare7']={}
-        buychange=int(fund['add'])+int(fund['amountChange']);
-        sellchange=round(float(fund['shareChange'])*gsz,2)
-        fund['fundamount'] = amountNew+buychange-sellchange
-        week=get_week()
-        fund['buyamount7'][week]=buychange
-        fund['buyshare7'][week]=round(buychange/gsz,2)
-        fund['amountChange'] = 0
-        fund['shareChange'] = 0
-        fund['yingli'] = yingli
-        fund['zhangfu'] = zhangfu
-        fund['fundName'] = name
-        fund['dwjz'] = dwjz
-        fund['holdShare'] = holdShare
-        fund['amountNow'] = amountNow
-        fund['gusuanzhi'] = gsz
-    fundconf['todayIncameSum'] = round(sum,2)
-    fundconf['gztime'] = gztime[0:10]
-    return fundconf
+    fundconfnew=new_fundconf()
+    for fund,url,amount in zip(fundconf['fundlist'],url_list,amountlist):
+        fundnew=get_NewFund(fund,url,amount)
+        fundconfnew['fundlist'].append(fundnew)
+    fundconfnew['todayIncameSum'] = round(sum,2)
+    fundconfnew['gztime'] = gztime[0:10]
+    return fundconfnew
 
 def main():
     fundconf=getfundconf()
     # 格式化json
-    strRes=str(fundconf).replace("'",'"')
-    strRes=strRes.replace(",",",\n\t")
-    strRes=strRes.replace("[","[\n\t")
-    strRes=strRes.replace("{","{\n\t")
-    strRes=strRes.replace("}","\n}")
+    strRes= json.dumps(fundconf, indent=4, separators=(',', ':'), ensure_ascii=False)
     filename=time.strftime(module_path+"\\..\\history\\%Y-%m-%d.json", time.localtime())
     with codecs.open(filename, 'w' ,"utf-8") as f:
         f.write(str(strRes))
